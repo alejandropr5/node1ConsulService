94e6b38e880fbbb2d5c184159d0bd918ff46f1d0
alejandropr5 <alejandropr05@hotmail.com>
2023-04-28 19:55:14 +0000

    Changing service ID

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 
 const SERVICE_NAME='mymicroservice';
-const SERVICE_ID='m'+process.argv[2];
+const SERVICE_ID='N1_'+process.argv[2];
 const SCHEME='http';
 const HOST='99.5.100.2';
 const PORT=process.argv[2]*1;
